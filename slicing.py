5ecc1345ae74520a6feeb6a7b97887aa2e8279ca
Priya7075 <102401793+Priya7075@users.noreply.github.com>
2022-04-01 14:39:39 +0530

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,52 @@
+# a="python"
+# print(a,type(a)) #str
+# print(a[0])#p
+# print("python"[0])#p
+# print("python"[1])#y
+# print("python"[2])#t
+# print("python"[3])#h
+# print("python"[4])#o
+# print("python"[5])#n
+# print("python"[6])#indexerror:string index out of range
+# v="department"
+# print(v[3]+v[2]+v[3]+v[4]+v[5]+v[6]+v[7]+v[8]+v[9])#apartment
+# print("python"[-1])#n
+# print("python"[-2])#o
+# print("python"[-3])#h
+# print("python"[-4])#t
+# print("python"[-5])#y
+# print("python"[-6])#p
+# print("python"[-7])#indexerror:string index out of range
+# w="python"
+# print(w[:])#python
+# print(w[0:])#python
+# print(w[0:6])#python
+# print(w[0:0])#blank line
+# print(w[0:1])#p
+# print(w[0:2])#py
+# print(w[0:3])#pyt
+# print(w[0:4])#pyth
+# print(w[0:5])#pytho
+# print(w[0:6])#python
+# print(w[0:7])#python
+# print(w[0:99])#python
+# print(w[0:0])#blank line
+# print(w[0:2])#py
+# print(w[0:3])#pyt
+# print(w[0:-1])#pytho
+# print(w[0:-2])#pyth
+# print(w[0:-3])#pyt
+# print(w[0:-4])#py
+# print(w[0:-5])#p
+# print(w[0:-6])#blank
+# print(w[0:-7])#blank
+# r="python"
+# print(r[2:5])#tho
+# print(r[3:4])#h       
+# print(r[5:2])#blank line
+# print(r[5:5])#blank line
+# y="python"
+# print(y[-1:-6])#blank line
+# print(y[2:-5])#blank line
+# print(y[-6:])#python
+
