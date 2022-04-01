0bfd90d19b9c13303b746f127cb7f6c83b01329a
Priya7075 <102401793+Priya7075@users.noreply.github.com>
2022-04-01 14:39:01 +0530

    Add files via upload

---
+++ slice.py
@@ -0,0 +1,26 @@
+c="python"
+# print(c[ : : ])#python
+# print(c[0 : : 1])#python
+# print(c[ : : 2])#pto
+# print(c[ : : 3])#ph
+# print(c[ : : 4])#po
+# print(c[ : : 5])#pn
+# print(c[ : : 6])#p
+# print(c[: : -1])#nohtyp
+# print(c[: : -2])#nhy
+# print(c[ : : -3])#nt
+# print(c[ : : -4])#ny
+# print(c[ : : -5])#np
+# print(c[ : : -6])#n
+# print(c[1: : ])#ython
+# print(c[2: : ])#thon
+# print(c[3: : ])#hon
+# print(c[4: : ])#on
+# print(c[5: : ])#n
+# print(c[6: : ])#blankline
+# print(c[1: 6: ])#ython
+# print(c[2: 5: ])#tho
+# print(c[3: 4: ])#h
+# print(c[4: 3: ])#blankline
+# print(c[5: 2: ])#blankline
+# print(c[6: 1: ])#blankline
